a6dd738f60e043325ad27b23e5121f5dcd613aa5
James Winters <winters@shh.mpg.de>
2019-10-28 09:27:42 +0100

    Delete runs.py

--- runs.py
+++
@@ -1,4 +0,0 @@
-import multiprocessing
-from ABM_winters import *
-
-simulation(n=100,generations=100,trans_param=1.0,optimization=0.6,exploration=0.2,directory='output.txt',run=0,out=False,pspace=[0.5,0.3,0.2])
